fac62094b664fa4a9cde9121292751d35954f32a
lpalomino7030 <i202224764@cibertec.edu.pe>
2026-05-25 00:50:10 -0500

    feat: implement sales page with image gallery and details, update routing and navigation links

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Sales } from './sales/sales';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'home', component: Home }
+  { path: 'home', component: Home },
+  {path:'sales', component: Sales }
 
 ];
