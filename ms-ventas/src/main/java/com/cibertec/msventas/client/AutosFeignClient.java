460b8a88c0dc7a59d085308252e35df6c6613ca6
luispal7030 <I202224764@cibertec.edu.pe>
2026-06-05 17:32:54 -0500

    configuracion

--- ms-ventas/src/main/java/com/cibertec/msventas/client/AutosFeignClient.java
+++ ms-ventas/src/main/java/com/cibertec/msventas/client/AutosFeignClient.java
@@ -1,5 +1,6 @@
 package com.cibertec.msventas.client;
 
+import com.cibertec.msventas.dto.AutoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-clientes")
 public interface AutosFeignClient {
     @GetMapping("/autos/{id}")
-    AutosResponse obtenerAuto(@PathVariable Long id);
+    AutoResponse obtenerAuto(@PathVariable Long id);
 
 }
--- ms-ventas/src/main/java/com/cibertec/msventas/client/ClienteFeignClient.java
+++ ms-ventas/src/main/java/com/cibertec/msventas/client/ClienteFeignClient.java
@@ -1,5 +1,6 @@
 package com.cibertec.msventas.client;
 
+import com.cibertec.msventas.dto.ClienteResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
