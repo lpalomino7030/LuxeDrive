f74dcd5bac42fff24ce4293b5ecb6ce52838fa7d
lpalomino7030 <I202224764@cibertec.edu.pe>
2026-06-06 18:13:05 -0500

    eureka server conected and ms active

--- ms-ventas/src/main/java/com/cibertec/msventas/client/AutosFeignClient.java
+++ ms-ventas/src/main/java/com/cibertec/msventas/client/AutosFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-clientes")
+@FeignClient(name = "ms-autos")
 public interface AutosFeignClient {
     @GetMapping("/autos/{id}")
     AutoResponse obtenerAuto(@PathVariable Long id);
--- ms-ventas/src/main/java/com/cibertec/msventas/entity/Venta.java
+++ ms-ventas/src/main/java/com/cibertec/msventas/entity/Venta.java
@@ -3,7 +3,6 @@ package com.cibertec.msventas.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "ventas")
+
 public class Venta {
 }
