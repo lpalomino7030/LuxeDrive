ebe98f34b65efed428308d777435e08c2841b5f5
luispal7030 <I202224764@cibertec.edu.pe>
2026-06-08 15:20:52 -0500

    pruebas bd

--- ms-ventas/database/DbVentas.sql
+++ ms-ventas/database/DbVentas.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE Ventas;
+
+CREATE TABLE VENTAS(
+    idVenta SERIAL NOT NULL,
+    idClientes INT NOT NULL,
+    idAutos INT NOT NULL,
+    fechaVenta DATE NOT NULL,
+    precioVenta DECIMAL(10,2) NOT NULL,
+    descripcion VARCHAR(250)
+)
+
+
