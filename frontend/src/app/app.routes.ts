c8b4df49290434540051666c2d98a073b7d78a68
luispal7030 <I202224764@cibertec.edu.pe>
2026-06-17 17:58:06 -0500

    se actualiza el modulo de frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './home/home';
 import { Sales } from './sales/sales';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
-  {path:'sales', component: Sales }
-
+  { path: 'sales', component: Sales },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
 ];
--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  username = '';
+  password = '';
+
+  constructor(private http: HttpClient) {}
+
+  login() {
+
+    const body = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post<any>(
+      'http://localhost:8088/auth/login',
+      body
+    ).subscribe({
+
+      next: (response) => {
+
+        localStorage.setItem(
+          'token',
+          response.token
+        );
+
+        alert('Login correcto');
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+        alert('Usuario o contraseña incorrectos');
+
+      }
+
+    });
+
+  }
+
+}
