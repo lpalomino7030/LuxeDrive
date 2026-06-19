04316747c9fb48779b0ebcbb37e6618c62bb4651
luispal7030 <I202224764@cibertec.edu.pe>
2026-06-19 17:29:33 -0500

    se genera servicio de autenticacion

---
+++ frontend/src/app/login/auth-service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private router: Router) {}
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getUser(): any {
+    const token = this.getToken();
+
+    if (!token) {
+      return null;
+    }
+
+    return jwtDecode(token);
+  }
+
+  getUsername(): string {
+    return this.getUser()?.sub ?? '';
+  }
+
+  getRol(): string {
+    return this.getUser()?.rol ?? '';
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('rol');
+
+    this.router.navigate(['/login']);
+  }
+}
