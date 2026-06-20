abe1280090c5f92de73b9facdb8d84838c3a7c74
lpalomino7030 <I202224764@cibertec.edu.pe>
2026-06-19 23:56:49 -0500

    la carpeta pages/public no se actualiaba por que esta observada por el gitignore por lo cual siempre perdia los cambio que habia trabajado en otra maquina debo tener en cuenta eso y volver a hacerlo todo.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
-import { Inicio } from './pages/public/inicio/inicio';
-import { Home } from './pages/public/home/home';
-import { Rent } from './pages/public/rent/rent';
-import { Sales } from './pages/public/sales/sales';
+import { Inicio } from './pages/landing/inicio/inicio';
+import { Home } from './pages/landing/home/home';
+import { Rent } from './pages/landing/rent/rent';
+import { Sales } from './pages/landing/sales/sales';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Client } from './pages/client/client';
-import { Nosotros } from './pages/public/Nosotros/nosotros';
+import { Nosotros } from './pages/landing/Nosotros/nosotros';
 import { Contenido } from './pages/dashboard/contenido/contenido';
 import { Panel } from './pages/client/panel/panel';
 
---
+++ frontend/src/app/pages/landing/headers/headers.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headers',
+  standalone: true,
+  imports: [],
+  templateUrl: './headers.html',
+  styleUrl: './headers.css',
+})
+export class Headers {}
