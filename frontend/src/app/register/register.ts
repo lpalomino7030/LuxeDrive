f0ede2e8d3277a99880418441e46fc1dc7013f1e
lpalomino7030 <I202224764@cibertec.edu.pe>
2026-06-22 21:36:47 -0500

    revision para registro. funcional

--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,9 +1,68 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-register',
-  imports: [],
+
+  standalone: true,
+
+  imports: [FormsModule],
+
   templateUrl: './register.html',
+
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  nombre = '';
+
+  apellido = '';
+
+  correo = '';
+
+  telefono = '';
+
+  password = '';
+
+  confirmarPassword = '';
+
+  constructor(private http: HttpClient) {}
+
+  register() {
+    if (this.password !== this.confirmarPassword) {
+      alert('Las contraseñas no coinciden');
+
+      return;
+    }
+
+    const body = {
+      username: this.correo,
+
+      password: this.password,
+
+      rol: 'CLIENTE',
+    };
+
+    this.http
+      .post(
+        'http://localhost:8080/auth/register',
+
+        body,
+      )
+      .subscribe({
+        next: () => {
+          alert('Cuenta creada correctamente');
+
+          window.location.href = '/login';
+        },
+
+        error: (err) => {
+          console.error(err);
+
+          alert('Error al registrar el usuario');
+        },
+      });
+  }
+}
